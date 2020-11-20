777a9f1600e2e80c4db1142a5d5a440b39d83fc0
Nkita <nikitalinnik22@gmail.com>
2020-11-20 10:52:12 +0200

    Функции, стрелочные функции

--- js/script.js
+++ js/script.js
@@ -1,36 +1,36 @@
 "use strict";
 
-const number0fFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let num = 20;
 
-const pesonalMovieDB = {
-    count: number0fFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+function showFirstMessage(text) {
+    console.log(text);
+    num = 10;
+}
+
+showFirstMessage('Hello World');
+console.log(num);
+
+// console.log(calc(4, 3));
+// console.log(calc(6, 5));
+// console.log(calc(9, 7));
+
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() {
+    console.log('Hello');
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
+logger();
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        pesonalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
-}
-
-if (pesonalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов');
-} else if (pesonalMovieDB.count >= 10 && pesonalMovieDB.count < 30) {
-    console.log('Вы классический зритель');
-} else if (pesonalMovieDB.count >= 30) {
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
-}
-
-console.log(pesonalMovieDB);
+const calc = (a, b) => a + b;
