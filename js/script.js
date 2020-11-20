8430227e76fcab8dc125573f7960512a8e1c9f6e
Nkita <nikitalinnik22@gmail.com>
2020-11-20 13:09:11 +0200

    Functions
    Optimized my code with functions

--- js/script.js
+++ js/script.js
@@ -1,17 +1,69 @@
 "use strict";
 
-const str = 'test';
+let number0fFilms;
 
-console.log(str.toUpperCase());
-console.log(str);
+function start() {
+    number0fFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const fruit = 'Some fruit';
+    while (number0fFilms == '' || number0fFilms == null || isNaN(number0fFilms)) {
+        number0fFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+ 
+start();
 
-console.log(fruit.indexOf('fruit'));
+const pesonalMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-const logg = 'Hello everyone';
 
-console.log(logg.substr(6, 8));
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            pesonalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }    
+}
 
-const test = '12.2px';
-console.log(parseInt(test));
+// rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (pesonalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (pesonalMovieDB.count >= 10 && pesonalMovieDB.count < 30) {
+        console.log('Вы классический зритель');
+    } else if (pesonalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
+
+// detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(pesonalMovieDB);
+    }
+}
+
+showMyDB(pesonalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        pesonalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    } 
+}
+
+writeYourGenres();
