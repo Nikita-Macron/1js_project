11770cd735c020f03fb05ba981b970fd9f760f88
Nkita <nikitalinnik22@gmail.com>
2020-11-20 09:26:46 +0200

    Условия(conditions)

--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
 "use strict";
 
-const number0fFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+if (4 == 9) {
+    console.log('Ok!');
+}  else {
+    console.log('Error');
+}
 
-const pesonalMovieDB = {
-    count: number0fFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// if (num <= 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Too much');
+// } else {
+//     console.log('Ok!');
+// }
 
-pesonalMovieDB.movies[a] = b;
-pesonalMovieDB.movies[c] = d;
+// (num == 50) ? console.log('Ok!') : console.log('Error');
 
-console.log(pesonalMovieDB);
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('Error');
+        break;
+    case 100:
+        console.log('Error');
+        break;
+    case 50:
+        console.log('True');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
