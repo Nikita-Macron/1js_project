0435d31ed10fd655b0cb68f99a556ce985881cb1
Nkita <nikitalinnik22@gmail.com>
2020-11-20 09:40:06 +0200

    Циклы(cycles)

--- js/script.js
+++ js/script.js
@@ -1,36 +1,24 @@
 "use strict";
 
-if (4 == 9) {
-    console.log('Ok!');
-}  else {
-    console.log('Error');
-}
+let num = 50;
 
-
-// if (num <= 49) {
-//     console.log('Error');
-// } else if (num > 100) {
-//     console.log('Too much');
-// } else {
-//     console.log('Ok!');
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
 // }
 
-// (num == 50) ? console.log('Ok!') : console.log('Error');
+// do {
+//     console.log(num);
+//     num++;
+// }
 
+// while (num <= 55);
 
-const num = 50;
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
 
-switch (num) {
-    case 49:
-        console.log('Error');
-        break;
-    case 100:
-        console.log('Error');
-        break;
-    case 50:
-        console.log('True');
-        break;
-    default:
-        console.log('Не в этот раз');
-        break;
+    console.log(i);
 }
