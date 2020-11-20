910a2fea8bf5bea864937be7119f68275dcedfd4
Nkita <nikitalinnik22@gmail.com>
2020-11-20 17:23:05 +0200

    Объекты, деструктуризация объектов

--- js/script.js
+++ js/script.js
@@ -1,26 +1,41 @@
 "use strict";
 
-function first() {
-    //Do something
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
+const button = {
+    name: 'mainButton',
+    bgColor: 'red',
+    border: 'black',
+    borderRadius: '10px',
+    sizes: {
+        width: '50px',
+        height: '15px'
+    },
+    makeTest: function() {
+        console.log('Test');
+    }
+};
+
+button.makeTest();
+
+const {width, height} = button.sizes;
+console.log(width);
+
+// console.log(Object.keys(button).length);
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+let counter = 0;
+for (let key in button) {
+    if (typeof(button[key]) === 'object') {
+        for (let i in button[key]) {
+            console.log(`Свойство ${i} имеет значение ${button[key][i]}`);
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${button[key]}`);
+        counter++;
+    } 
 }
 
-function second() {
-    console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
-
-function done() {
-    console.log('Я прошел этот урок');
-}
-
-learnJS('JavaScript', done);
+console.log(counter);
