3ad3aeb1b9addedd8b98e9b478db111a1faf9ee5
Nkita <nikitalinnik22@gmail.com>
2020-11-21 16:50:02 +0200

    Получение элементов со страницы

--- js/script.js
+++ js/script.js
@@ -1,52 +1,21 @@
 "use strict";
 
-// To string
+const box = document.getElementById('box');
 
-// 1)
-console.log(typeof(String(null)));
+console.log(box);
 
-// 2)
-console.log(typeof(5 + '')); // Лучший метод
+const btns = document.getElementsByTagName('button');
 
-const num = 5;
+console.log(btns[1]);
 
-console.log('https://vk.com/catalog/' + num);
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
 
-const fontSize = 26 + 'px';
+const hearts = document.querySelectorAll('.heart');
 
-// To number
+hearts.forEach(item => {
+    console.log(item);
+});
 
-// 1)
-console.log(typeof(Number('4')));
-
-// 2)
-console.log(typeof(+'4')); // Лучший метод
-
-// 3)
-console.log(typeof(parseInt('15px', 10)));
-
-
-let answ = +prompt('Hello', '');
-
-// To boolean
-
-// 0, '', null, undefined, Nan;  <=  Это все превращается в false
-
-// 1)
-let switcher = null;
-
-if (switcher) {
-    console.log('Working...');
-}
-
-switcher = 1;
-
-if (switcher) {
-    console.log('Working...');
-}
-
-// 2)
-console.log(typeof(Boolean('4')));
-
-// 3)
-console.log(typeof(!!('4')));
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
